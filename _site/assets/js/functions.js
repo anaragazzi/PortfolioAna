54ece39d42492a8464caafe333bbf6854419917d
Ana Ragazzi <ana.sragazzi@gmail.com>
2016-01-19 12:38:31 +0000

    portfolio

---
+++ _site/assets/js/functions.js
@@ -0,0 +1,71 @@
+jQuery(function() {
+	$(document).ready(function(){
+		console.log("jQuery OK!");
+		smoothScroll(600);
+		workBelt();
+		workLoad();
+		showElement();
+	});
+});
+
+// smoothScroll function is applied from the document ready function
+function smoothScroll (duration) {
+	$('a[href^="#"]').on('click', function(event) {
+
+	    var target = $( $(this).attr('href') );
+	    
+	    if( target.length ) {
+	        event.preventDefault();
+	        $('html, body').animate({scrollTop: target.offset().top}, duration);
+	    }
+	});
+}
+
+
+function workBelt() {
+
+	$('.thumb-unit').click(function() {
+		$('.work-belt').css('left','-100%');
+		$('.work-container').show();
+	});
+	
+	$('.work-return').click(function() {
+		$('.work-belt').css('left','0%');
+		$('.work-container').hide(800);
+	});
+	
+	$('.work-return2').click(function() {
+		$('.work-belt').css('left','0%');
+		$('.work-container').hide(800);
+	});
+
+}
+
+function  workLoad() {
+  
+  $.ajaxSetup({ cache: false });
+  
+  $('.thumb-unit').click(function() {
+  
+    var $this = $(this),
+        newTitle = $this.find('span').text(),
+        newfolder = $this.data('folder'),
+        spinner = '<div class="loader">Loading...</div>',
+        newHTML = '/work/'+ newfolder +'.html';
+      
+    $('.project-load').html(spinner).load(newHTML);
+    $('.project-title').text(newTitle);
+  });
+  
+}
+
+
+function showElement() {
+
+	$('div.textcontent').hide();
+	$('a.text').bind('click', function() {
+	    $('div.textcontent').fadeOut(0);
+	    $('#'+$(this).attr('id')+'content').delay(100).fadeIn(800);
+	});  
+
+};
